c349ab09378aa526e6c4ebc0357ed8b66fd82f7c
Sudipta <hisudipta26@gmail.com>
2022-08-14 18:33:32 +0530

    style added

--- src/components/SubNav.jsx
+++ src/components/SubNav.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 const SubNav = () => {
   return (
     <>
-      <div style={{ display: "flex", flexDirection: "row" }}>
+      <div style={{ display: "flex", flexDirection: "row", padding: "0.5rem 0rem"}}>
         <Link to="/" style={{ textDecoration: "none" }}>
           Mentors
         </Link>
-        <Link to="/">Partners</Link>
-        <Link to="/">Investors</Link>
-        <Link to="/">Incubators</Link>
-        <Link to="/">Women Entrepreneurs</Link>
+        <Link to="/" style={{ textDecoration: "none" }}>Partners</Link>
+        <Link to="/" style={{ textDecoration: "none" }}>Investors</Link>
+        <Link to="/" style={{ textDecoration: "none" }}>Incubators</Link>
+        <Link to="/" style={{ textDecoration: "none" }}>Women Entrepreneurs</Link>
         <input
           type="text"
           placeholder="search anything"
