a51cc51103d5af162686451ac336bdc20eeb4fa2
Aritra-art <aritrachowdhury65@gmail.com>
2022-08-25 18:17:45 +0530

    changed

---
+++ src/pages/MentorLanding.jsx
@@ -0,0 +1,5 @@
+const MentorLanding = () => {
+
+}
+
+export default MentorLanding;
--- src/pages/Partners.js
+++ src/pages/Partners.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "../pages/Card";
 import "../pages/Card.css";
 import "./Mentors.css";
+import {useState} from 'react';
 // import Brajesh from "../imgs/market/Brajesh_Maheshwari.jpg";
 // import Vinay from "../imgs/Vinay-kumar-reddy.jpg";
 // import Prasant from "../imgs/Dr P Prasant.jpg";
@@ -10,11 +11,27 @@ import "./Mentors.css";
 // import Karan from "../imgs/market/Karan Shah.jpg";
 
 function Partners(props) {
+  const [filter,setFilter] = useState('')
   return (
     <div>
       <h1 className="card__heading">
-        Find Your <span style={{ color: "#3e7ddb" }}>Partners</span>
+        Look For <span style={{ color: "#3e7ddb" }}>Partners</span>
       </h1>
+      <div style={{display:"flex",alignItems:"right", justifyContent:"right" , marginRight:"40px"}}>
+      <select
+      style={{height:"2rem", width:"10vw", color:"#3e7ddb", outline:"none", backgroundColor:"#f0f9ff",
+    borderRadius:"0.5rem", fontWeight:"bold", border:"2px solid #3e7ddb", cursor:"pointer"
+    }}
+      onChange = {(e)=>{
+        setFilter(e.target.value)
+      }}
+      >
+        <option value="government" >Government</option>
+        <option value="corporate">Corporate</option>
+        <option value="globalbodies">Global Bodies</option>
+       
+      </select>
+      </div>
       <div className="card-container">
         {/* <Card
           img={Brajesh}
@@ -65,48 +82,4 @@ function Partners(props) {
 
 export default Partners;
 
-// import React from "react";
-// import PartnersCard from "./PartnersCard";
-// import Corporates from "../imgs/Corporate.png";
-// import Government from "../imgs/government.png";
-// import Global from "../imgs/global.jpg";
 
-// const Partners = () => {
-//   return (
-//     <div>
-//       <h1
-//         style={{
-//           color: "#3e7ddb",
-//           textAlign: "center",
-//         }}
-//       >
-//         Connect with Partners
-//       </h1>
-//       <div
-//         style={{
-//           display: "grid",
-//           gridTemplateColumns: "repeat(auto-fit, minmax(15rem, 24rem))",
-//           gap: "2rem",
-//           justifyContent: "center",
-//           paddingTop: "2rem",
-//           paddingBottom: "2rem",
-//         }}
-//       >
-//         <PartnersCard
-//           img={Corporates}
-//           title="Corporates"
-//         />
-//         <PartnersCard
-//           img={Government}
-//           title="Government"
-//         />
-//         <PartnersCard
-//           img={Global}
-//           title="Global Bodies"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Partners;
