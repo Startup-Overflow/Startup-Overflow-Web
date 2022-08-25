f27beffcb4bb775f64191f8c6da15b943fcd5bff
Aritra-art <aritrachowdhury65@gmail.com>
2022-08-25 23:46:52 +0530

    View Profile

--- src/pages/ViewProfile.js
+++ src/pages/ViewProfile.js
@@ -1,21 +1,22 @@
 import MentorProfile from "./MentorProfile";
-// import Brajesh from "../imgs/Brajesh_Maheshwari.jpg";
+import C from "../imgs/agriculture/c.jpg";
 
 function ViewProfile() {
   return (
     <MentorProfile
-      // img={Brajesh}
-      // name="Brajesh Maheshwari"
-      // desc="Co-Founder Director of Allen Career Institute"
-      // btnName="Book for Appointment"
-      // availability="Monday to Saturday- 11 AM to 1 PM"
-      // experience="1.5 Years"
-      // specialization={[
-      //   "Corporate Strategy",
-      //   "Fund Raise Strategies",
-      //   "Business Planning",
-      // ]}
-      // bio="A Corporate Strategy expert with over 15 years of experience in successfully executing both large and small projects with particular focus on Financial Services industry. Experience in handling successful Fund Raises, Mergers & Acquisitions, Product launches, Investor Relations and Product Design & Development."
+      img={C}
+      name="Brajesh Maheshwari"
+      desc="Co-Founder Director of Allen Career Institute"
+      btnName="Book for Appointment"
+      availability="Monday to Saturday- 11 AM to 1 PM"
+      experience="1.5 Years"
+      location="Ahmedabad, Gujarat"
+      specialization={[
+        "Corporate Strategy",
+        "Fund Raise Strategies",
+        "Business Planning",
+      ]}
+      bio="I have completed MBA from IIM Ahmedabad in 1995. Corporate Strategy expert with over 15 years of experience in successfully executing both large and small projects with particular focus on Financial Services industry. Experience in handling successful Fund Raises, Mergers & Acquisitions, Product launches, Investor Relations and Product Design & Development."
     />
   );
 }
